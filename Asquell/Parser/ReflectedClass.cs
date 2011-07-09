d5d796229b90b80bf2bb26c27c3ab3881875022c
James Turner <j-man-77@hotmail.com>
2011-07-09 19:28:23 +0930

    Added ReflectClass.cs and ReflectCommands.cs. Allows in-script reflection to C#

---
+++ Asquell/Parser/ReflectedClass.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Asquell.Objects;
+using System.Reflection;
+
+namespace Asquell.Parser
+{
+    public class ReflectedClass
+    {
+        private Type _type;
+        private Dictionary<string,MethodInfo> _usableMethods;
+        public ReflectedClass(Type type)
+        {
+            _type = type;
+            _usableMethods = new Dictionary<string, MethodInfo>();
+            MethodInfo[] methods = type.GetMethods(BindingFlags.Public | BindingFlags.Static);
+            ParameterInfo[] pinfo;
+            bool allowedMethod;
+            for (int i = 0; i < methods.Length; i++)
+            {
+                allowedMethod = true;
+                pinfo = methods[i].GetParameters();
+                for (int i2 = 0; i2 < pinfo.Length; i2++)
+                {
+                    if (pinfo[i2].IsOut||pinfo[i2].ParameterType != typeof(AsquellObj))
+                    {
+                        allowedMethod = false; break;
+                    }
+                }
+                if (allowedMethod)
+                    _usableMethods.Add(methods[i].Name,methods[i]);
+            }
+        }
+        public bool DoReflection(string name, AsquellObj[] args)
+        {
+            if (_usableMethods.ContainsKey(name))
+            {
+                _usableMethods[name].Invoke(null, args);
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Asquell/Parser/ReflectedCommands.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Asquell.Invokables;
+using System.Reflection;
+using Asquell.Objects;
+
+namespace Asquell.Parser
+{
+    public class ReflectedCommands
+    {
+        private Dictionary<string, ReflectedClass> _opToClass;
+        public ReflectedCommands()
+        {
+            _opToClass = new Dictionary<string, ReflectedClass>();
+        }
+        public bool Embed(Type type)
+        {
+            bool success = false;
+            if (!_opToClass.ContainsKey(type.Name)&&type.IsClass&&type.IsPublic)
+            {
+                _opToClass[type.Name] = new ReflectedClass(type);
+            }
+            return success;
+        }
+        public bool CallReflection(string embedName, string methodName, params AsquellObj[] args)
+        {
+            if (_opToClass.ContainsKey(embedName))
+            {
+                _opToClass[embedName].DoReflection(methodName, args);
+                return true;
+            }
+            return false;
+        }
+    }
+}
