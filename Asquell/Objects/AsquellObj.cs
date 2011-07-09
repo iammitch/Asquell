76c429da1df6b29ebbdf0aa4bea563c9ddbed43b
James Turner <j-man-77@hotmail.com>
2011-07-09 16:35:19 +0930

    Added Type Defining to AsquellObj

--- Asquell/Objects/AsquellObj.cs
+++ Asquell/Objects/AsquellObj.cs
@@ -7,28 +7,44 @@ namespace Asquell.Objects
 {
     public enum AsquellObjectType
     {
-        Undefined = 0,
-        Null = 1,
-        String = 2,
-        Number = 3,
-        Boolean = 4,
-        Generic = 5,
-        RunTimeValue = 6,
+        Null = 0,
+        String = 1,
+        Number = 2,
+        Boolean = 3,
+        RunTimeValue = 4,
+        MathsOperator = 5,
+        BooleanOperator = 6,
         Invalid = 99
     }
     public class AsquellObj
     {
-        private AsquellObjectType type = AsquellObjectType.Undefined;
+        private AsquellObjectType type = AsquellObjectType.Invalid;
         private string rawValue;
-        private string parseValue; //Mainly for strings where % was changed
+        private object parseValue;
         public AsquellObj(string rawValue)
         {
             this.rawValue = rawValue;
-            this.parseValue = rawValue;
             defineType();
+        }
+        private int findStringEnd(string str, int start, char stringChar)
+        {
+            int end = -1;
+            while (true)
+            {
+                if (start + 1 >= str.Length) return -1;
+                end = str.IndexOf(stringChar, start + 1);
+                if (end == -1)
+                    return -1;
+                else if (str[end - 1] == '\\')
+                    continue;
+                else
+                    return end;
+            }
         }
         private void defineType()
         {
+            #region Type String-like
+
             int isString = rawValue.IndexOfAny(new char[] { '\'', '"' });
             if (isString!=-1)
             {
@@ -45,8 +61,10 @@ namespace Asquell.Objects
                 for (int i = 0; i < rawValue.Length; i++)
                 {
                     tmp = rawValue[i];
-                    if (tmp == '"' || tmp == '\'')
+                    if ((tmp == '"' || tmp == '\''))
                     {
+                        if (i >= 1 && rawValue[i-1] == '\\')
+                            continue;
                         if (tmp == inString)
                         {
                             inString = ' ';
@@ -56,7 +74,8 @@ namespace Asquell.Objects
                             inString = tmp;
                             stringStart = i;
 
-                            int endIndex = rawValue.IndexOf(inString, stringStart + 1);
+                            int endIndex = findStringEnd(rawValue, stringStart, inString);
+
                             if (endIndex == -1)
                             { type = AsquellObjectType.Invalid; return; }
 
@@ -69,11 +88,80 @@ namespace Asquell.Objects
                             if (i + 1 != rawValue.Length)
                             { type = AsquellObjectType.Invalid; return; }
 
+                            type = AsquellObjectType.String;
                         }
                     }
                 }
             }
+
+            #endregion
+
+            #region Type Numeric
+
+            double numericValue;
+            if (double.TryParse(rawValue,out numericValue))
+            {
+                parseValue = numericValue;
+                type = AsquellObjectType.Number;
+                return;
+            }
+
+            #endregion
+
+            #region Type Boolean
+
+            bool booleanValue;
+            if (bool.TryParse(rawValue, out booleanValue))
+            {
+                parseValue = booleanValue;
+                type = AsquellObjectType.Boolean;
+                return;
+            }
+
+            #endregion
+
+            #region Type Null
+
+            if (rawValue == "NULL")
+            {
+                type = AsquellObjectType.Null;
+                parseValue = null;
+                return;
+            }
+
+            #endregion
+
+            #region Type Maths Operators
             
+            if (rawValue.IndexOfAny(new char[] { '+', '-', '*', '/', '%', '^'})==0&&rawValue.Length==1)
+            {
+                type = AsquellObjectType.MathsOperator;
+                parseValue = rawValue;
+                return;
+            }
+            
+            #endregion
+
+            #region Type Boolean Operators
+
+            if ((rawValue.IndexOfAny(new char[] { '>', '<' }) == 0 && rawValue.Length == 1) ||
+                ((rawValue.IndexOf("<=") == 0 || rawValue.IndexOf("<=") == 0 ||
+                    rawValue.IndexOf("==") == 0 || rawValue.IndexOf("!=") == 0) && rawValue.Length == 2))
+            {
+                type = AsquellObjectType.BooleanOperator;
+                parseValue = rawValue;
+            }
+            
+            #endregion
+
+            #region Type Runtime Value
+
+            if (!rawValue.Contains(' '))
+            {
+                type = AsquellObjectType.RunTimeValue;
+            }
+
+            #endregion
         }
     }
 }
