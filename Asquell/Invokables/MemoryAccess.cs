b1895b7d6f3384fc1455f373cd852a6a9488a983
James Turner <j-man-77@hotmail.com>
2011-07-10 13:34:08 +0930

    Added Move to MemoryAccess. Fixed some errors.

--- Asquell/Invokables/MemoryAccess.cs
+++ Asquell/Invokables/MemoryAccess.cs
@@ -7,7 +7,7 @@ using Asquell.Objects;
 
 namespace Asquell.Invokables
 {
-    [AsquellClass(AccessibleName="MEM")]
+    [AsquellClass(AccessibleName=null)]
     public static class MemoryAccess
     {
         [AsquellMethod(AccessibleName = "Set", Exposed = true, NoMemoryBlock = false)]
@@ -16,9 +16,19 @@ namespace Asquell.Invokables
             memory.ModifyVariable(to, from);
         }
         [AsquellMethod(AccessibleName = "Delete", Exposed = true, NoMemoryBlock = false)]
-        public static void SetMemory(MemoryBlock memory, AsquellObj block)
+        public static void DeleteMemory(MemoryBlock memory, AsquellObj block)
         {
             memory.DeleteVariable(block);
+        }
+        [AsquellMethod(AccessibleName = "Move", Exposed = true, NoMemoryBlock = false)]
+        public static void MoveMemory(MemoryBlock memory, AsquellObj from, AsquellObj to)
+        {
+            if (from.Type == AsquellObjectType.RunTimeValue && to.Type == AsquellObjectType.RunTimeValue)
+            {
+                AsquellObj rawValue = memory.GetRealVariable(from);
+                memory.ModifyVariable(to, rawValue);
+                memory.DeleteVariable(from);
+            }
         }
     }
 }
